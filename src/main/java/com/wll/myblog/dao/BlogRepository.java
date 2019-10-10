c96434af88f790d76ac7b3655a40d7650347daf5
wll <570157656@qq.com>
2019-10-10 11:58:03 +0800

    归档功能

--- src/main/java/com/wll/myblog/dao/BlogRepository.java
+++ src/main/java/com/wll/myblog/dao/BlogRepository.java
@@ -21,4 +21,11 @@ public interface BlogRepository extends JpaRepository<Blog,Long>, JpaSpecificati
     @Modifying
     @Query("update Blog b set b.views = b.views + 1 where b.id = ?1")
     void updateViews(Long id);
+
+    @Query("select function('date_format',b.updateTime,'%Y') as year from Blog b group by function('date_format',b.updateTime,'%Y') order by year desc ")
+    List<String> findGroupYear();
+
+    @Query("select b from Blog b where function('date_format',b.updateTime,'%Y') = ?1")
+    List<Blog> findByYear(String year);
+
 }
--- src/main/java/com/wll/myblog/service/impl/BlogServiceImpl.java
+++ src/main/java/com/wll/myblog/service/impl/BlogServiceImpl.java
@@ -18,14 +18,8 @@ import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import javax.persistence.criteria.CriteriaBuilder;
-import javax.persistence.criteria.CriteriaQuery;
-import javax.persistence.criteria.Predicate;
-import javax.persistence.criteria.Root;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
-import java.util.Map;
+import javax.persistence.criteria.*;
+import java.util.*;
 
 @Service
 public class BlogServiceImpl implements BlogService {
@@ -83,7 +77,15 @@ public class BlogServiceImpl implements BlogService {
 
     @Override
     public Page<Blog> listBlog(Long tagId, Pageable pageable) {
-        return null;
+
+        return blogRepository.findAll(new Specification<Blog>() {
+            @Override
+            public Predicate toPredicate(Root<Blog> root, CriteriaQuery<?> criteriaQuery, CriteriaBuilder criteriaBuilder) {
+                Join join = root.join("tags");
+                return criteriaBuilder.equal(join.get("id"),tagId);
+            }
+        },pageable);
+
     }
 
     @Override
@@ -100,12 +102,17 @@ public class BlogServiceImpl implements BlogService {
 
     @Override
     public Map<String, List<Blog>> archiveBlog() {
-        return null;
+        List<String> years = blogRepository.findGroupYear();
+        Map<String, List<Blog>> map = new HashMap<>();
+        for (String year : years) {
+            map.put(year,blogRepository.findByYear(year));
+        }
+        return map;
     }
 
     @Override
     public Long countBlog() {
-        return null;
+        return blogRepository.count();
     }
 
     @Override
